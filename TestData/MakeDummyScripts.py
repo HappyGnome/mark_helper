78c2ad093b5d0f41f17e4d7e087261bf3c0a3c57
HappyGnome <bencorp20002000@yahoo.co.uk>
2020-05-19 09:33:03 +0100

    Tidy up test data generation.

--- TestData/MakeDummyScripts.py
+++ TestData/MakeDummyScripts.py
@@ -10,78 +10,94 @@ import string
 import os
 import subprocess as sp
 
-#Attempt to generate N random strings
-def gen_filenames(N,strlen=8):
-    ret=[]
+
+# Attempt to generate N random strings
+def gen_filenames(N, strlen=8):
+    ret = []
     for n in range(N):
-        name=''.join((random.choice(string.ascii_uppercase+string.digits) for j in range(strlen)))
-        if not name in ret: ret.append(name)
+        name = ''.join((random.choice(string.ascii_uppercase+string.digits)
+                        for j in range(strlen)))
+        if name not in ret:
+            ret.append(name)
     return ret
-#15, 15, 15
-questions={'1':{'a':2,'b':3,'c':5,'d':5},'2':{'a':8,'b':2,'c':5},'3':{'a':3,'b':3,'c':4,'d':5}}#{question_name:[part_max_marks]}
-paper_width=21
-paper_height=29.7
-minmax_pages_per_q_part=[0.5,2]
 
-stu_nums=gen_filenames(4)
-prob_miss_part=0.1
 
-#{stu_num:{q_name:mark}}
-stu_marks={}
+# 15, 15, 15
+# {question_name:[part_max_marks]}
+questions = {'1': {'a': 2, 'b': 3, 'c': 5, 'd': 5},
+             '2': {'a': 8, 'b': 2, 'c': 5},
+             '3': {'a': 3, 'b': 3, 'c': 4, 'd': 5}}
+paper_width = 21
+paper_height = 29.7
+minmax_pages_per_q_part = [0.5, 2]
 
-#get header for tex file
-headerlines=[]
-with open('dummy_header.txt','r') as f:
-    headerlines=f.readlines()
+stu_nums = gen_filenames(4)
+prob_miss_part = 0.1
 
-def skip_pages(at,file):
-    #file.write("\\begin{mpage}\n")
+# {stu_num:{q_name:mark}}
+stu_marks = {}
+
+# get header for tex file
+headerlines = []
+with open('dummy_header.txt', 'r') as f:
+    headerlines = f.readlines()
+
+
+def skip_pages(at, file):
+    # file.write("\\begin{mpage}\n")
     for m in range(int(at)):
-       file.write("\\end{mpage}\n\\begin{mpage}\n")
+        file.write("\\end{mpage}\n\\begin{mpage}\n")
 
-    return at%1
-#change coordinate systems and add margins at top/botom of page
+    return at % 1
+
+
+# change coordinate systems and add margins at top/botom of page
 def at2actual(at):
     return 0.95-at*0.9
 
-for s in stu_nums:
-    stu_marks[s]={}
-    filepath=os.path.join('DummyScripts',s+'.tex')
-    with open(filepath,'w') as f:
-        f.writelines(headerlines)
-        at=0#position (as proportion of a page) in script file modulo 1 page
 
+for s in stu_nums:
+    stu_marks[s] = {}
+    filepath = os.path.join('DummyScripts', s+'.tex')
+    with open(filepath, 'w') as f:
+        f.writelines(headerlines)
+        # position (as proportion of a page) in script file modulo 1 page
+        at = 0
 
         f.write("\\begin{mpage}\n")
         for q in questions:
-            mark=0#track mark as it's generated
-            at+=0.1
-            at=skip_pages(at,f)
-            f.write("\\mnoth{{{}}}{{{}}} {{\\Large Question {}}}\n".format(0.2*paper_width,at2actual(at)*paper_height, q))
+            mark = 0  # track mark as it's generated
+            at += 0.1
+            at = skip_pages(at, f)
+            f.write("\\mnoth{{{}}}{{{}}} {{\\Large Question {}}}\n"
+                    .format(0.2*paper_width, at2actual(at)*paper_height, q))
             for part in questions[q]:
-                if random.random()>prob_miss_part:
-                    at+=0.03
-                    at=skip_pages(at,f)
-                    f.write("\\mnoth{{{}}}{{{}}} {{({})}}\n".format(0.2*paper_width,at2actual(at)*paper_height, part))
-                    at+=random.uniform(*minmax_pages_per_q_part)
-                    at=skip_pages(at,f)
-                    part_mark=int(random.random()*questions[q][part])
-                    mark+=part_mark
-                    f.write("\\mnoth{{{}}}{{{}}} {{...Work worth {} marks}}\n"\
-                            .format(random.uniform(0.1*paper_width,0.8*paper_width),at2actual(at)*paper_height, part_mark))
-            stu_marks[s][q]=mark
-        f.write("\\end{mpage}\n\end{document}")
+                if random.random() > prob_miss_part:
+                    at += 0.03
+                    at = skip_pages(at, f)
+                    f.write("\\mnoth{{{}}}{{{}}} {{({})}}\n"
+                            .format(0.2*paper_width,
+                                    at2actual(at)*paper_height, part))
+                    at += random.uniform(*minmax_pages_per_q_part)
+                    at = skip_pages(at, f)
+                    part_mark = int(random.random()*questions[q][part])
+                    mark += part_mark
+                    f.write("\\mnoth{{{}}}{{{}}} {{...Work worth {} marks}}\n"
+                            .format(random.uniform(0.1*paper_width,
+                                                   0.8*paper_width),
+                                    at2actual(at)*paper_height, part_mark))
+            stu_marks[s][q] = mark
+        f.write("\\end{mpage}\n\\end{document}")
 
 
 os.chdir("DummyScripts")
-for s in stu_nums:#compile examples
-    sp.run(["pdflatex",s+".tex","-aux-directory=./aux_files"])
+for s in stu_nums:  # compile examples
+    sp.run(["pdflatex", s+".tex", "-aux-directory=./aux_files"])
 os.chdir("..")
 
 
-
-#save correct marks
-with open('mark_check.csv','w') as f:
+# save correct marks
+with open(os.path.join('DummyScripts', 'mark_check.csv'), 'w') as f:
     f.write("Student #")
     for q in questions:
         f.write(",{}".format(q))
